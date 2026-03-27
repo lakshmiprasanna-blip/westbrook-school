8a71b49d5eab150857080615597b136382086625
Prasanna <lakshmiprasanna@8views.com>
2026-03-27 17:25:05 +0530

    configg

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    localPatterns: [
+    remotePatterns: [
       {
-        pathname: '/assets/blogs/**',
-      },
-      {
-        pathname: '/assets/**',  // covers logo and all other images
+        protocol: 'https',
+        hostname: 'westbrookinternational.com',
+        pathname: '/assets/**',
       },
     ],
   },
